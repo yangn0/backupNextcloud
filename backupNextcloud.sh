20622bc45c8c9915b8b81a73bf5b93f778dfdb3a
Ning Yang <yangn0@qq.com>
2026-02-11 02:28:08 +0800

    change: backupDir

--- backupNextcloud.sh
+++ backupNextcloud.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+backupDir=/home/yangn0/disk1T/nextcloudBackups
+mkdir -p $backupDir
+
 startTime=`date`
 startTime_s=`date +%s`
 echo ------------------------------------------------------------------
 echo $startTime
 
 # delete old backups
-cd /home/yangn0/disk500G/nextcloudBackups
-if (($(ls -l | grep "d" | wc -l) >= 1))
+cd $backupDir
+if (($(ls -l | grep "d" | wc -l) >= 2))
 then
     file_array=($(ls))
     rm -rf ${file_array[0]}
@@ -16,13 +19,13 @@ fi
 
 docker exec --user www-data -i nextcloud php occ maintenance:mode --on || (echo 'Nextcloud开启维护模式失败' && exit)
 cd /home/yangn0/disk6T/
-mkdir /home/yangn0/disk500G/nextcloudBackups/nextcloud-${startTime_s}
-tar -zcf - nextcloud | split -d -b 20G - /home/yangn0/disk500G/nextcloudBackups/nextcloud-${startTime_s}/nextcloud-${startTime_s}.tgz.
+mkdir $backupDir/nextcloud-${startTime_s}
+tar -zcf - nextcloud | split -d -b 20G - $backupDir/nextcloud-${startTime_s}/nextcloud-${startTime_s}.tgz.
 echo nextcloud-${startTime_s} '打包成功'
 docker exec --user www-data -i nextcloud php occ maintenance:mode --off
 
 cd /home/yangn0/aliyunpan/
-backup_dir=/home/yangn0/disk500G/nextcloudBackups/nextcloud-${startTime_s}
+backup_dir=$backupDir/nextcloud-${startTime_s}
 for file in `ls $backup_dir`
   do
     ./aliyunpan upload ${backup_dir}/$file /NASbackups/nextcloudBackups/nextcloud-${startTime_s}
